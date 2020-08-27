5c99d39f267a6762ea6a251f215ff957b45b4caa
Snikerso <42245963+Snikerso@users.noreply.github.com>
2020-08-27 18:18:59 +0200

    add cors

--- tytool/tytool/settings.py
+++ tytool/tytool/settings.py
@@ -37,18 +37,26 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'announcements',
+
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+
 ]
 
 ROOT_URLCONF = 'tytool.urls'
