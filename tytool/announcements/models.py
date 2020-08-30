a2593c80a5cdbc4fac5617a8ae1412431a374c74
Snikerso <42245963+Snikerso@users.noreply.github.com>
2020-08-30 18:48:03 +0200

    change announcement model

--- tytool/announcements/models.py
+++ tytool/announcements/models.py
@@ -1,20 +1,34 @@
 from django.db import models
-
-
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 
 class AnnouncmentsScienceTool(models.Model):
     title = models.TextField(max_length=32,blank=False,null=False)
-    windowss = models.TextField(max_length=32,blank=False,null=False)
-
-    
-    def __str__(self):
-        return self.title
-
-
-class AnnouncmentsArtTool(models.Model):
-    title = models.TextField(max_length=32,blank=False,null=False)
     description = models.TextField(max_length=32,blank=False,null=False)
-
+    announcementType = models.TextField(default=False , max_length=4,blank=False,null=False)
+    author =  models.ForeignKey(get_user_model(),to_field='username', on_delete=models.CASCADE)
+    createAt = models.DateTimeField(default=timezone.now)
+    isWorker = models.BooleanField(default=False)
+    isHelper = models.BooleanField(default=False)
+    isPayment = models.BooleanField(default=False)
+    timeToEnd = models.CharField(default=False, max_length=20, blank=True)
+    place = models.CharField(default=False , max_length=30)
+    tags = models.TextField(default=False , max_length=100,blank=True)
+    linkFirstName =  models.CharField(default=False, max_length=20, blank=True)
+    linkFirst = models.TextField(default=False , max_length=100,blank=True)
+    linkSecond = models.TextField(default=False , max_length=100,blank=True)
+    linkSecondName =  models.CharField(default=False, max_length=20, blank=True)
+    
+    # zapisani użytkownicy 
+    # ulubione
+    # zainteresowani
+    # opis1 , opis2 , opis3 
+    # tak sie zastanaiwam czy kazdy typ Announcment science tool powinien miec osobny model
+    
+    
     
     def __str__(self):
         return self.title
+
+
--- tytool/announcements/serializers.py
+++ tytool/announcements/serializers.py
@@ -1,11 +1,21 @@
 
 from rest_framework import serializers
 from .models import AnnouncmentsScienceTool
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
 
-
-
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id','username','password']
+        extra_kwargs = {'password': {'write_only':True, 'required':True}} # To robi tak że nie pobiera passwordu przy zapytaniu ale jest konieczny do założenia nowego
+        
+    def create(self,validated_data): 
+        user = User.objects.create_user(**validated_data) 
+        Token.objects.create(user=user) # tworzy token dla każdaego użytkownika
+        return user
 
 class AnnouncmentsScienceToolSerializer(serializers.ModelSerializer):
     class Meta:
         model = AnnouncmentsScienceTool
-        fields = ['id','title']
+        fields = '__all__'
